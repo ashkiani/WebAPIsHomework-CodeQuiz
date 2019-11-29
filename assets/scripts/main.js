f1639a6bfddd708386b93370d61105f2fec0c073
Sia <ashkiani@yahoo.com>
2019-11-29 16:15:41 -0500

    Added main.js and moved some of the code from question.js into main.js - Also renamed question.js to questions.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,3 @@
-var questions = [
-  {
-    title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts"
-  },
-  {
-    title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses"
-  },
-  ///etc.
-  {
-    title: "What is the HTML tag under which one can write the JavaScript code?",
-    choices: ["javascript", "scripted", "script", "js"],
-    answer: "script"
-  }
-
-];
-
 var timeEl = document.querySelector(".navbar-brand");
 var questionDivEl = document.querySelector("#divQuestion");
 var questionTextEl = document.querySelector("#pQuestion");
@@ -178,7 +158,6 @@ if (btn) {
 
   });
 }
-
 
 
 
---
+++ assets/scripts/questions.js
@@ -0,0 +1,21 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  ///etc.
+  {
+    title: "What is the HTML tag under which one can write the JavaScript code?",
+    choices: ["javascript", "scripted", "script", "js"],
+    answer: "script"
+  }
+
+];
+
+
