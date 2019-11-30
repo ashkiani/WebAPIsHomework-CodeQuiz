21628f2c6a7526dc0daa352d2db199e04e76a0c8
Sia <ashkiani@yahoo.com>
2019-11-29 20:30:59 -0500

    replaced the prompt box with a textbox on the form.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,9 +8,11 @@ var HomeLinkEl = document.querySelector("#homeLink");
 var btn = document.querySelector(".btn");
 var timerPreText = "Timer: ";
 var bTestInProgress = false;
+var bIsEnteringName = false;
 var timerInterval;
 const quizTime = 50;
 var secondsLeft = quizTime;
+var result;
 
 var qIndex = 0;
 var correctAnswersCount = 0;
@@ -31,7 +33,7 @@ function configurePage(IsTestInProgress) {
   else {
     timeEl.textContent = "Time over!";
     deleteExistingChoices();
-    btn.textContent = "Start";
+    //btn.textContent = "Start";
     resetTime();
     viewHSLinkEl.setAttribute("href", "index.html");
     HomeLinkEl.setAttribute("href", "scores.html");
@@ -43,19 +45,17 @@ function endTest() {
   clearInterval(timerInterval);
   configurePage(false);
   //ask user's name and record the score
+  bIsEnteringName = true;
+  //calculate the score
+  result = { "score": correctAnswersCount, "time": (quizTime - remaining) };
+  // questionTextEl.textContent = "Correct Answers: " + result.score + " time: " + result.time;
+  questionTextEl.innerHTML = "Correct Answers: " + result.score + " time: " + result.time + "<br> Enter your initial and click the Next button to store your result.";
 
-  var init = prompt("Enter your initial:");
-  if (init !== null) {
-    init = init.trim();
-    if (init.trim !== "") {
-      //calculate the score
-      var result = {"score": correctAnswersCount, "time": (quizTime - remaining) };
+  var input = document.createElement('input');
+  input.type = "text";
+  input.setAttribute("id", "init");
+  answersDivEl.appendChild(input);
 
-      localStorage.setItem(init, JSON.stringify(result));
-    }
-  }
-  questionTextEl.textContent = "Try to answer the following code related questions within the time limit. Keep in mind that an incorrect answer will decrease your remaining time by 5 seconds."
-  return;
 }
 
 function ShowQuestion() {
@@ -71,19 +71,6 @@ function ShowQuestion() {
       divRadio.appendChild(lblChoice)
       answersDivEl.appendChild(divRadio);
 
-      // var chkAnswer =document.createElement("div");
-      //   chkAnswer.setAttribute("id","form-check");
-      //   answersDivEl.appendChild(chkAnswer);
-      //   var inp =document.createElement("input");
-      //   inp.setAttribute("id","materialUnchecked");
-      //   inp.setAttribute("class","form-check-input");
-      //   inp.setAttribute("type","checkbox");
-      //   chkAnswer.appendChild(inp);
-      //   var lbl=document.createElement("label");
-      //   lbl.setAttribute("class","form-check-label");
-      //   lbl.setAttribute("for","materialUnchecked");
-      //   lbl.innerHTML=question.choices[i];
-      //   chkAnswer.appendChild(lbl);
     }
 
   }
@@ -144,16 +131,26 @@ function deleteExistingChoices() {
 
 if (btn) {
   btn.addEventListener("click", function (event) {
-    //event.preventDefault;
+    event.preventDefault;
     if (bTestInProgress) {
       processAnswer();
       deleteExistingChoices();
       qIndex++;
       ShowQuestion();
     }
-    else {
+    else if (bIsEnteringName) {
+      var init = document.querySelector("#init").value;
+      init = init.trim();
+      if (init !== null && init !== "") {
+        localStorage.setItem(init, JSON.stringify(result));
+      } else {
+        console.log("User didn't enter initials. Score won't be recorded");
+      }
+      bIsEnteringName = false;
+      location.reload();
+      document.location.href = "scores.html";
+    } else {
       startTest();
-
     }
 
   });
